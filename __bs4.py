4a15dfbd6c3e256916f5927b94bb561e98d92cb5
burakyilmaz55 <burakyilmz17@gmail.com>
2020-09-12 22:27:18 +0300

    mesaj

--- __bs4.py
+++ __bs4.py
@@ -1,19 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
+import random
 
 url = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
 
 html = requests.get(url).content
 soup = BeautifulSoup(html, 'html.parser')
 
-list = soup.find('tbody', {"class":"lister-list"}).find_all('tr',limit=249)
-count = 0
+list = soup.find('tbody', {"class":"lister-list"}).find_all('tr')
+sayi = random.randint(1,250)
+movies = []
 
 for tr in list:
     title = tr.find("td",{"class":"titleColumn"}).find("a").text
     year = tr.find("td",{"class":"titleColumn"}).find("span").text.strip("()")
     rating = tr.find("td",{"class":"ratingColumn"}).find("strong").text
-    count+=1
-    print(f"{count}-Movie: {title}\n   Year:{year}\n   Rating:{rating}")
+    movies.append([title,year,rating])
 
-
+randoMovie = movies[sayi]
+print(f"Film Adı: {randoMovie[0]}\n Vizyon Yılı: {randoMovie[1]}\nIMDB Puanı: {randoMovie[2]}")
